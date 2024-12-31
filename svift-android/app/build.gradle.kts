40bd957a656eb145720a5e108f3bdf7761dc2ab4
athulantony95 <154115716+athulantony95@users.noreply.github.com>
2024-12-31 11:05:16 +0530

    v1.11

--- svift-android/app/build.gradle.kts
+++ svift-android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "athul.svift.android"
         minSdk = 26
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.10"
+        versionCode = 11
+        versionName = "1.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
